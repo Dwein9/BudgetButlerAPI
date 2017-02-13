1fb7b95424b56119dceeb1193d9c0f1913b182b3
franco83-2 <franco.84@gmail.com>
2017-02-13 16:23:40 -0500

    jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_pamas)
+    if @user.save
+      jwt = Auth.encrypt({user_id: @user.id})
+      render json: {jwt: jwt, @user.name}
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -25,6 +24,7 @@ module BudgetButlerAPI
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.autoload_paths << Rails.root.join('lib')
     config.api_only = true
   end
 end

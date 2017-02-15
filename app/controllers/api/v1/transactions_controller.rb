9bf6dae24354150df85a9a4d8121b45c6964448c
franco83-2 <franco.84@gmail.com>
2017-02-15 09:12:19 -0500

    sorted dates

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -4,13 +4,13 @@ module Api
 class TransactionsController < ApplicationController
 
   def index
-    render json: User.first.transactions.to_json
+    render json: User.first.transactions.order(day: :desc).to_json
   end
 
   def create
     value = transaction_params[:value].to_i
     Transaction.create(name: transaction_params[:name], day: transaction_params[:day], value: value, user_id: 1, expense_id: 1)
-    render json: User.first.transactions.to_json
+    render json: User.first.transactions.order(day: :desc).to_json
   end
 
   private

bd6e57d185398b47fee15259f1499565cacd7f44
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-15 15:52:09 -0500

    sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    binding.pry
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
       jwt = Auth.encrypt({user_id: @user.id})

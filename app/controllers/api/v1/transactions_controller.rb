90771cbb2c66089160f94364c8b18ba67699a7df
franco83-2 <franco.84@gmail.com>
2017-02-15 16:38:37 -0500

    typo

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -7,7 +7,7 @@ class TransactionsController < ApplicationController
     if current_user
       render json: User.first.transactions.order(day: :desc).to_json
     else
-      reutn json: {message: "You are not logged in", status_code: 421}
+      return json: {message: "You are not logged in", status_code: 421}
     end
   end
 

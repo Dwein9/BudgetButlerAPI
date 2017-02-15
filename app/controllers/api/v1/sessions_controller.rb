fef9249ec71949f0548c1221298c3977ffa04bce
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-15 15:28:04 -0500

    fixed sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password])
-      jwt = Auth.issue({user_id: @user.id})
+    binding.pry
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
+      jwt = Auth.encrypt({user_id: @user.id})
       render json: {jwt: jwt}
     else
       render json: {message: "Incorrect Email or Password", status_code: 422}
     end
+  end
+
+  def session_params
+    params.require(:user).permit(:email, :password)
   end
 
 end

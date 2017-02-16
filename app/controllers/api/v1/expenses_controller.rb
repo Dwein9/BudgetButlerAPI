699bdafb4fa02e93aefad36b07a938e5bf34421e
franco83-2 <franco.84@gmail.com>
2017-02-16 17:30:39 -0500

    fixed expense controller

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -12,7 +12,7 @@ class ExpensesController < ApplicationController
   def create
     if logged_in?
       value = expense_params[:value].to_f
-      expense = Expense.new(category: transaction_params[:category], budget: transaction_params[:budget])
+      expense = Expense.new(category: expense_params[:category], budget: expense_params[:budget])
       expense.user = current_user
       expense.save
       render json: @current_user.expenses.order(budget: :desc).to_json
@@ -22,8 +22,8 @@ class ExpensesController < ApplicationController
   def update
     if logged_in?
       value = expense_params[:value].to_f
-      expense = Expense.find(transaction_params[:id])
-      expense.update(category: expenses_params[:category], budget: expenses_params[:budget])
+      expense = Expense.find(expense_params[:id])
+      expense.update(category: expense_params[:category], budget: expense_params[:budget])
       expense.save
       render json: @current_user.expenses.order(budget: :desc).to_json
     end
@@ -40,7 +40,7 @@ class ExpensesController < ApplicationController
   private
 
   def expense_params
-    params.require(:transaction).permit(:category, :budget, :id)
+    params.require(:expense).permit(:category, :budget, :id)
   end
 end
 

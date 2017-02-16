596af6a5f80c328f60c5abcee4b856c32a50937a
David Weinstein <txea@Davids-MacBook-Air.local>
2017-02-16 16:23:16 -0500

    integers to floats

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -11,7 +11,7 @@ class TransactionsController < ApplicationController
 
   def create
     if logged_in?
-      value = transaction_params[:value].to_i
+      value = transaction_params[:value].to_f
       transaction = Transaction.new(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: 1)
       transaction.user = current_user
       transaction.save
@@ -21,7 +21,7 @@ class TransactionsController < ApplicationController
 
   def update
     if logged_in?
-      value = transaction_params[:value].to_i
+      value = transaction_params[:value].to_f
       transaction = Transaction.find(transaction_params[:id])
       transaction.update(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: 1)
       transaction.save
@@ -31,7 +31,7 @@ class TransactionsController < ApplicationController
 
   def destroy
     if logged_in?
-      transaction = Transaction.find(transaction_params[:id])
+      transaction = Transaction.find(params[:id])
       transaction.destroy
       render json: @current_user.transactions.order(day: :desc).to_json
     end

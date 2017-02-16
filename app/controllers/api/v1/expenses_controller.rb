88d98283a9f3d0f10cd757037e9eb65d01bce910
franco83-2 <franco.84@gmail.com>
2017-02-16 17:12:37 -0500

    added expense controller

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -4,9 +4,44 @@ module Api
 class ExpensesController < ApplicationController
 
   def index
-    render json: User.first.expenses.to_json
+    if logged_in?
+      render json: @current_user.expenses.order(budget: :desc).to_json
+    end
   end
 
+  def create
+    if logged_in?
+      value = expense_params[:value].to_f
+      expense = Expense.new(category: transaction_params[:category], budget: transaction_params[:budget])
+      expense.user = current_user
+      expense.save
+      render json: @current_user.expenses.order(budget: :desc).to_json
+    end
+  end
+
+  def update
+    if logged_in?
+      value = expense_params[:value].to_f
+      expense = Expense.find(transaction_params[:id])
+      expense.update(category: expenses_params[:category], budget: expenses_params[:budget])
+      expense.save
+      render json: @current_user.expenses.order(budget: :desc).to_json
+    end
+  end
+
+  def destroy
+    if logged_in?
+      expense = Expense.find(expense_params[:id])
+      expense.destroy
+      render json: @current_user.expenses.order(budget: :desc).to_json
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:transaction).permit(:category, :budget, :id)
+  end
 end
 
 end

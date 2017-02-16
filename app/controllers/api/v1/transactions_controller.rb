2a57cfcd6fcc8dd0361f2182a0577df1e6d599ff
franco83-2 <franco.84@gmail.com>
2017-02-16 13:17:43 -0500

    edit transaction controller update

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -5,22 +5,34 @@ class TransactionsController < ApplicationController
 
   def index
     if logged_in?
-      render json: User.first.transactions.order(day: :desc).to_json
+      render json: @current_user.transactions.order(day: :desc).to_json
     end
   end
 
   def create
-    value = transaction_params[:value].to_i
-    transaction = Transaction.new(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: 1)
-    transaction.user = current_user
-    transaction.save
-    render json: User.first.transactions.order(day: :desc).to_json
+    if logged_in?
+      value = transaction_params[:value].to_i
+      transaction = Transaction.new(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: 1)
+      transaction.user = current_user
+      transaction.save
+      render json: @current_user.transactions.order(day: :desc).to_json
+    end
+  end
+
+  def update
+    if logged_in?
+      value = transaction_params[:value].to_i
+      transaction = Transaction.find(transaction_params[:id])
+      transaction.update(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: 1)
+      transaction.save
+      render json: @current_user.transactions.order(day: :desc).to_json
+    end
   end
 
   private
 
   def transaction_params
-    params.require(:transaction).permit(:name, :value, :day, user_id: 1)
+    params.require(:transaction).permit(:name, :value, :day, :id)
   end
 
 end

77384fbcc76bd5f6fd7dd6f7d35ee3dcebf67035
github username <zylberberg.jonathan@gmail.com>
2017-02-19 18:24:47 -0500

    added income stuff

--- app/controllers/api/v1/incomes_controller.rb
+++ app/controllers/api/v1/incomes_controller.rb
@@ -7,6 +7,40 @@ class IncomesController < ApplicationController
     render json: User.first.incomes.to_json
   end
 
+   def create
+    if logged_in?
+      value = income_params[:value].to_f
+      income = Income.new(name: income_params[:name], amount: income_params[:amount])
+      income.user = current_user
+      income.save
+      render json: @current_user.incomes.order(amount: :desc).to_json
+    end
+  end
+
+  def update
+    if logged_in?
+      value = income_params[:value].to_f
+      income = Income.find(income_params[:id])
+      income.update(name: income_params[:name], amount: income_params[:amount])
+      income.save
+      render json: @current_user.incomes.order(amount: :desc).to_json
+    end
+  end
+
+  def destroy
+    if logged_in?
+      income = Income.find(params[:id])
+      income.destroy
+      render json: @current_user.incomes.order(amount: :desc).to_json
+    end
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:name, :amount, :id)
+  end
+
 end
 
 end

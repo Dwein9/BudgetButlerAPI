8792dcc32577e05ba72b60c485105db787cf317e
github username <zylberberg.jonathan@gmail.com>
2017-02-21 22:06:12 -0500

    updated transaction controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -9,13 +9,20 @@ class TransactionsController < ApplicationController
     end
   end
 
+# params[:transaction][:month].to_i
+
   def create
     if logged_in?
       value = transaction_params[:value].to_f
       transaction = Transaction.new(name: transaction_params[:name], day: transaction_params[:day], value: value,  expense_id: transaction_params[:expense_id])
       transaction.user = current_user
       transaction.save
-      render json: @current_user.transactions.order(day: :desc).to_json
+
+      selected_month = (params[:transaction][:month].to_i + 1)
+      user_transaction = Transaction.where(user_id: @current_user)
+      transactions_month = user_transaction.where('day >= ? AND day <= ?', DateTime.new(2017,selected_month,1).beginning_of_month, DateTime.new(2017,selected_month,1).end_of_month)
+
+      render json: transactions_month.order(day: :desc).to_json
     end
   end
 
@@ -25,15 +32,26 @@ class TransactionsController < ApplicationController
       transaction = Transaction.find(transaction_params[:id])
       transaction.update(name: transaction_params[:name], day: transaction_params[:day], value: value, expense_id: transaction_params[:expense_id])
       transaction.save
-      render json: @current_user.transactions.order(day: :desc).to_json
+      
+
+      selected_month = (params[:transaction][:month].to_i + 1)
+      user_transaction = Transaction.where(user_id: @current_user)
+      transactions_month = user_transaction.where('day >= ? AND day <= ?', DateTime.new(2017,selected_month,1).beginning_of_month, DateTime.new(2017,selected_month,1).end_of_month)
+
+      render json: transactions_month.order(day: :desc).to_json
     end
   end
 
-  def destroy
+  def delete
     if logged_in?
-      transaction = Transaction.find(params[:id])
+      transaction = Transaction.find(params[:transaction][:id])
       transaction.destroy
-      render json: @current_user.transactions.order(day: :desc).to_json
+
+      selected_month = (params[:transaction][:month].to_i + 1)
+      user_transaction = Transaction.where(user_id: @current_user)
+      transactions_month = user_transaction.where('day >= ? AND day <= ?', DateTime.new(2017,selected_month,1).beginning_of_month, DateTime.new(2017,selected_month,1).end_of_month)
+
+      render json: transactions_month.order(day: :desc).to_json
     end
   end
 
